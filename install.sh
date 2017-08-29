742fa253fa1ecf129242b3c21871b5a47ff6e053
DarkCaster <dark.caster@outlook.com>
2017-08-29 03:09:13 +0300

    install.sh: set owner and permissions for installed files

--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ prefix="$1"
 [[ -z "$prefix" ]] && prefix="/usr/local"
 
 mkdir -p "$prefix/lib/systemd/system-generators"
-cp "$script_dir/crypttab-umount-generator.sh" "$prefix/lib/systemd/system-generators"
-
 mkdir -p "$prefix/bin"
-cp "$script_dir/crypttab-umount.sh" "$prefix/bin"
 
+cp "$script_dir/crypttab-umount-generator.sh" "$prefix/lib/systemd/system-generators"
 sed -i "s|__PREFIX__|$prefix|g" "$prefix/lib/systemd/system-generators/crypttab-umount-generator.sh"
+chown root:root "$prefix/lib/systemd/system-generators/crypttab-umount-generator.sh"
+chmod 750 "$prefix/lib/systemd/system-generators/crypttab-umount-generator.sh"
+
+cp "$script_dir/crypttab-umount.sh" "$prefix/bin"
+chown root:root "$prefix/bin/crypttab-umount.sh"
+chmod 750 "$prefix/bin/crypttab-umount.sh"
+
 systemctl daemon-reload
